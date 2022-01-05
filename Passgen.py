077854ab2180be46953da2446e30b774ae0fb586
ItzNeorus <88227422+ItzNeorus@users.noreply.github.com>
2022-01-05 19:43:59 +0700

    Add files via upload
    Add new function!

--- Passgen.py
+++ Passgen.py
@@ -5,7 +5,7 @@ import sys
 
 config = {}
 
-with open('config.json', 'r') as r: # Load data in config.
+with open('config.json', 'r', encoding='UTF-8') as r: # Load data in config.
 	config = json.load(r)
 
 i = 0 # Index for arguments.
@@ -18,13 +18,18 @@ for p in sys.argv: # Arguments initilization. Example: python Passgen.py (First 
 		try:
 			p = int(p)
 		except:
-			p = float(p)
+			try:
+				p = float(p)
+			except:
+				p = list(p)
 		if i == 1:
 			config['lengthFirst'] = p
 		elif i == 2:
 			config['lengthSecond'] = p
 		elif i == 3:
 			config['asciiSymbolChance'] = p
+		elif i == 4:
+			config['unicalSymbolChance'] = p
 
 if config['lengthFirst'] > config['lengthSecond']: # Error initilization.
 	print('Error (Invalid value) in config.json(lengthFirst).')
@@ -37,9 +42,12 @@ pwg = ''
 for i in range(random.randint(config['lengthFirst'], config['lengthSecond'])): # Genering password.
 	chance = random.random() # Ascii symbol chance.
 	if chance < config['asciiSymbolChance']: # Insert ascii symbol.
-		symbols = string.ascii_lowercase
+		symbols = string.ascii_letters
 		symbol = random.choice(symbols) # Random ascii symbol.
 		pwg = pwg + symbol # Gluing.
+		if chance < config['unicalSymbolChance']:
+			symbol = random.choice(config['unicalSymbols'])
+			pwg = pwg + symbol
 	else:
 		symbol = random.randint(0, 9) # Random number.
 		symbol = str(symbol)
